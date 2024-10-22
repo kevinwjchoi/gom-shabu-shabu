0bf7a166ee09d7c9596ebbc39bbffdab4bcfdb10
Kevin Choi <kevinnwjchoi@gmail.com>
2024-10-22 16:44:14 -0400

    updated events. added a new box for reservations

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -14,12 +14,20 @@ const Events = () => {
         <div className="eventpageoverlay">
           <Container>
             <Box textAlign="center" py={5}>
-              <Typography variant="h2" gutterBottom>
-                Special event coming up?
-              </Typography>
-              <Typography variant="body1">
-                Join us for an unforgettable culinary experience!
-              </Typography>
+                <Typography variant="h6"
+                sx={{
+                    fontWeight: 100,
+                    fontSize: { xs: '2rem', md: '4rem' }, // Adjusted for mobile
+                    color: 'white',
+                    textAlign: 'center', // Center text
+                    padding: { xs: 2, md: 0 }, // Responsive padding
+                }}
+                >
+                    Special event coming up?
+                </Typography>
+                <Typography variant="body1">
+                    Join us for an unforgettable culinary experience!
+                </Typography>
             </Box>
           </Container>
         </div>
@@ -30,14 +38,38 @@ const Events = () => {
         <div className="eventpageoverlay">
           <Container>
             <Box textAlign="center" py={5}>
-              <Typography variant="h2" gutterBottom>
-                Don't Miss Out!
-              </Typography>
-              <Typography variant="body1">
-                Explore our exclusive menu for the event!
-              </Typography>
+                <Typography variant="h6"
+                sx={{
+                    fontWeight: 100,
+                    fontSize: { xs: '2rem', md: '4rem' }, // Adjusted for mobile
+                    color: 'white',
+                    textAlign: 'center', // Center text
+                    padding: { xs: 2, md: 0 }, // Responsive padding
+                }}
+                >
+                    Our long tables can accomodate a party of 12.
+                </Typography>
+                <Typography variant="body1">
+                    We also offer a closed off section for more private parties! 
+                </Typography>
             </Box>
           </Container>
+          <Box
+            sx={{
+            mt: 3,
+            padding: 2,
+            backgroundColor: '#f5f5f5',
+            borderRadius: 1,
+            textAlign: 'center',
+            }}
+          >
+            <Typography variant="h5" sx={{ mb: 2 }}>
+              Call us to make reservations
+            </Typography>
+            <Typography variant="body1">
+              Make sure to provide us with: Name, Date, Time, # of Guests, and Phone Number
+            </Typography>
+          </Box>
         </div>
       </div>
     </div>
