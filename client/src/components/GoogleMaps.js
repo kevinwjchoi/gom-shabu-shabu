cceeede58d032420d4ca377ba4ee76b8b36c50cc
Kevin Choi <kevinnwjchoi@gmail.com>
2024-10-26 15:52:37 -0400

    testing googlemaps comp

--- client/src/components/GoogleMaps.js
+++ client/src/components/GoogleMaps.js
@@ -1,22 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoogleMap, LoadScript, InfoWindow, Marker } from '@react-google-maps/api';
 
-const GoogleMaps = ({ address, location }) => {
-    const markerOffset = {
-        lat: location.lat - 0.00021, // Offset latitude
-        lng: location.lng // Keep the longitude the same
-      };
+const GoogleMaps = ({ address }) => {
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const markerOffset = location ? {
+    lat: location.lat - 0.00021, // Offset latitude
+    lng: location.lng // Keep the longitude the same
+  } : null;
+
+  useEffect(() => {
+    const fetchLocationData = async () => {
+      try {
+        const response = await fetch(`/api/gom-shabu-search`);
+        const data = await response.json();
+        
+        if (data.status === 'OK') {
+          // Assuming you want the first result's location
+          const result = data.results[0];
+          setLocation({
+            lat: result.geometry.location.lat,
+            lng: result.geometry.location.lng,
+          });
+        } else {
+          console.error('Error fetching location:', data.error_message);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchLocationData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div style={{ height: '100vh', width: '100%' }}>
       <LoadScript
-        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} // Optional: Use it only for Map styling, not for the search
       >
         <GoogleMap
           mapContainerStyle={{ height: '100%', width: '100%' }}
           zoom={18}
           center={location}
         >
-        <Marker position={markerOffset} />
+          {markerOffset && <Marker position={markerOffset} />}
           <InfoWindow position={location}>
             <div style={{ fontSize: '12px', color: '#000000' }}>
               <h1>Gom Shabu</h1>
